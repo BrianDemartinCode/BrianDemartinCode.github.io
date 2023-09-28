1fd937465921b48ac270353b9c8fb9cf227a66a7
Brian <117228535+BrianDemartinCode@users.noreply.github.com>
2023-09-28 12:34:54 -0300

    Add files via upload

---
+++ backend/catalogo.js
@@ -0,0 +1,118 @@
+//! PRODUCTOS DE HOMBRES
+const productosHombre = [
+  { nombre: "M03 Yd Yves D’Orgeval Parfums Masculino Horse 90 ML ", precio: "$5.140,00", imagen: "./img/img-perfumesMasculinos/1.jpeg", url:"https://wa.link/jsalni"},
+  { nombre: "M04 Yd Yves D’Orgeval Parfums Masculino Horse Blue 90 ML ", precio: "$5.140,00", imagen: "./img/img-perfumesMasculinos/2.jpeg", url:"https://wa.link/cb84i5"},
+  { nombre: "M05 Yd Yves D’Orgeval Parfums Masculino Horse Black 80 ML ", precio: "$4.935,00", imagen: "./img/img-perfumesMasculinos/3.jpeg", url:"https://wa.link/r1v5cm"},
+  { nombre: "M06 Yd Yves D’Orgeval Parfums Masculino Horse Red 90 ML ", precio: "$5.140,00", imagen: "./img/img-perfumesMasculinos/4.jpeg", url:"https://wa.link/gkk1oa"},
+  { nombre: "M07 Yd Yves D’Orgeval Parfums Masc. Horse Red Intense 80 ML ", precio: "$4.935,00", imagen: "./img/img-perfumesMasculinos/5.jpeg", url:"https://wa.link/63x8ic"},
+  { nombre: "M08 Yd Yves D’Orgeval Parfums Masc. Horse Supreme Oud 80 ML ", precio: "$4.935,00", imagen: "./img/img-perfumesMasculinos/6.jpeg", url:"https://wa.link/5ldepp"},
+  { nombre: "M09 Yd Yves D’Orgeval Parfums Orixa Pour Homme – 100 ML ", precio: "$5.665,00", imagen: "./img/img-perfumesMasculinos/7.jpeg", url:"https://wa.link/2g82mc"},
+  { nombre: "M10 Yd Yves D’Orgeval Parfums Masculino D’Orgeval Code – 100 ML ", precio: "$5.465,00", imagen: "./img/img-perfumesMasculinos/8.jpeg", url:"https://wa.link/h8vz2f"},
+  { nombre: "M11 Yd Yves D’Orgeval Parfums Unisex Yd One – 100 ML ", precio: "$5.465,00", imagen: "./img/img-perfumesMasculinos/9.jpeg", url:"https://wa.link/y6e0or"},
+  { nombre: "M12 Yd Yves D’Orgeval Parfums Masculino Gh Men – 100 ML ", precio: "$5.510,00", imagen: "./img/img-perfumesMasculinos/10.jpeg", url:"https://wa.link/9yzoqb"},
+  { nombre: "M13 Yd Yves D’Orgeval Parfums Masculino Gh Classic – 100 ML ", precio: "$5.350,00", imagen: "./img/img-perfumesMasculinos/11.jpeg", url:"https://wa.link/yhdgwo"},
+  { nombre: "M14 Yd Yves D’Orgeval Parfums Masculino Jenko – 100 ML ", precio: "$5.545,00", imagen: "./img/img-perfumesMasculinos/12.jpeg", url:"https://wa.link/3xn7e6"},
+  { nombre: "M16 Yd Yves D’Orgeval Parfums Masculino Hydo Red – 100 ML ", precio: "$5.350,00", imagen: "./img/img-perfumesMasculinos/13.jpeg", url:"https://wa.link/m0czva"},
+  { nombre: "M17 Yd Yves D’Orgeval Parfums Masc. Hydo Just Different – 100 ML ", precio: "$5.350,00", imagen: "./img/img-perfumesMasculinos/14.jpeg", url:"https://wa.link/9pcacv"},
+  { nombre: "M18 Yd Yves D’Orgeval Parfums Masculino Hydo – 100 ML ", precio: "$4.980,00", imagen: "./img/img-perfumesMasculinos/15.jpeg", url:"https://wa.link/wfevbi"},
+  { nombre: "M20 Yd Yves D´Orgeval Parfums Masculino Celsius – 100 ML ", precio: "$5.350,00", imagen: "./img/img-perfumesMasculinos/16.jpeg", url:"https://wa.link/c7ohb2"},
+  { nombre: "M21 Yd Yves D’Orgeval Parfums Masculino Acqua D’ Ydo – 100 ML ", precio: "$5.350,00", imagen: "./img/img-perfumesMasculinos/17.jpeg", url:"https://wa.link/nvvjsc"},
+  { nombre: "M24 Yd Yves D’Orgeval Parfums  Masculino Gh Prive – 100 ML ", precio: "$5.565,00", imagen: "./img/img-perfumesMasculinos/18.jpeg", url:"https://wa.link/hn539v"},
+  { nombre: "M26 Yd Yves D’Orgeval Parfums Masculino Black Yd  – 100 ML ", precio: "$5.665,00", imagen: "./img/img-perfumesMasculinos/19.jpeg", url:"https://wa.link/ui2d6j"},
+  { nombre: "M27 Yd Yves D’Orgeval Parfums Masc. Black Aphrodisiaque – 100ML ", precio: "$5.665,00", imagen: "./img/img-perfumesMasculinos/20.jpeg", url:"https://wa.link/4f26hb"},
+  { nombre: "M28 Yd Yves D’Orgeval Parfums Masculino Black Yd L’Elixir – 100 ML   ", precio: "$5.925,00", imagen: "./img/img-perfumesMasculinos/21.jpeg", url:"https://wa.link/ro7lp4"},
+  { nombre: "M30 Yd Yves D’Orgeval Parfums Yd12 MEN (Especial) – 100 ML ", precio: "$5.965,00", imagen: "./img/img-perfumesMasculinos/22.jpeg", url:"https://wa.link/kleetk"},
+  { nombre: "M31 Yd Yves D’Orgeval Parfums Yd12 Sexy Men (Especial) – 60 ML ", precio: "$5.965,00", imagen: "./img/img-perfumesMasculinos/23.jpeg", url:"https://wa.link/f92sv5"},
+  { nombre: "M32 Yd Yves D’Orgeval Parfums Yd12 Vip Night Club Men – 110 ML ", precio: "$6.465,00", imagen: "./img/img-perfumesMasculinos/24.jpeg", url:"https://wa.link/x2xs8g"},
+  { nombre: "M33 Yd Yves D’Orgeval Parfums Yd12 Vip Men Cristal – 110 ML ", precio: "$6.465,00", imagen: "./img/img-perfumesMasculinos/25.jpeg", url:"https://wa.link/bknoys"},
+  { nombre: "M34  Victorious – 100 ML - Parfums Vip Men - Fragance - 2022 ", precio: "$6.340,00", imagen: "./img/img-perfumesMasculinos/26.jpeg", url:"https://wa.link/41tttt"},
+  { nombre: "M37 Yd Yves D’Orgeval Parfums Masc.Victorious Intense- 100ML ", precio: "$6.340,00", imagen: "./img/img-perfumesMasculinos/27.jpeg", url:"https://wa.link/g66cuf"},
+  { nombre: "M38  Yd Yves D’Orgeval Parfums Masc.Yd Ange (Especial)  – 100 ML ", precio: "$5.755,00", imagen: "./img/img-perfumesMasculinos/28.jpeg", url:"https://wa.link/5n3yh3"},
+  { nombre: "M39 Yd Yves D’Orgeval Parfums Masc. G. Millionaire Gold – 100 ML ", precio: "$6.465,00", imagen: "./img/img-perfumesMasculinos/29.jpeg", url:"https://wa.link/gl81ej"},
+  { nombre: "M40 Yd Yves D’Orgeval Parfums Masculino G. Millionaire Prive – 100 ml - Frangance ", precio: "$6.465,00", imagen: "./img/img-perfumesMasculinos/30.jpeg", url:"https://wa.link/ieih22"},
+];
+
+function mostrarTarjetas() {
+  const contenedorTarjetas = document.getElementById("contenedor-tarjetas");
+
+  let tarjetasHTML = "";
+  productosHombre.forEach((producto) => {
+    tarjetasHTML += `
+    <div class="centerCard">
+  <div class="card" style="width: 18rem;">
+    <img src="${producto.imagen}" class="card-img-top" alt="...">
+    <div class="card-body body-center">
+      <h5 class="card-title fuente">${producto.nombre}</h5>
+      <p class="card-text">${producto.precio}</p>
+      <a href="${producto.url}" class="btn btn-primary button-color">Comprar</a>
+    </div>
+  </div>
+</div>
+        `;
+  });
+
+  contenedorTarjetas.innerHTML = tarjetasHTML;
+}
+
+window.addEventListener("load", mostrarTarjetas);
+//! PRODUCTOS DE HOMBRES
+
+// ! search
+const form = document.getElementById('ctn-search');
+const input = document.getElementById('input-search');
+const resultsContainer = document.getElementById('results');
+const modal = document.getElementById('myModal');
+const closeModalButton = document.getElementById('closeModal');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const searchTerm = input.value.toLowerCase();
+  const filteredProducts = productosHombre.filter(producto => {
+    const productName = producto.nombre.toLowerCase();
+    return productName.includes(searchTerm);
+  });
+
+  displayResults(filteredProducts);
+  openModal();
+});
+
+closeModalButton.addEventListener('click', closeAndClearModal);
+
+function displayResults(products) {
+  resultsContainer.innerHTML = '';
+
+  if (products.length === 0) {
+    resultsContainer.textContent = 'No se encontraron productos.';
+  } else {
+    products.forEach(producto => {
+      const productCard = document.createElement('div');
+      productCard.classList.add('product-card');
+
+      const productImage = document.createElement('img');
+      productImage.src = producto.imagen;
+
+      const productName = document.createElement('h2');
+      productName.textContent = producto.nombre;
+
+      const productPrice = document.createElement('p');
+      productPrice.textContent = `Precio: ${producto.precio}`;
+
+      productCard.appendChild(productImage);
+      productCard.appendChild(productName);
+      productCard.appendChild(productPrice);
+
+      resultsContainer.appendChild(productCard);
+    });
+  }
+}
+
+function openModal() {
+  modal.style.display = 'block';
+}
+
+function closeAndClearModal() {
+  modal.style.display = 'none';
+  resultsContainer.innerHTML = '';
+}
+// ! search
---
+++ backend/catalogofemenino.js
@@ -0,0 +1,118 @@
+//! PRODUCTOS FEMENINO
+const productosHombre = [
+    { nombre: "F03 Yd Yves D’Orgeval Femenino GH L`eau – 50 ML ", precio: "$5.250,00", imagen: "./img/img-perfumesFemeninos/1.jpeg", url:"https://wa.link/o6k4ux"},
+    { nombre: "F04 Yd Yves D’Orgeval Parfums Femenino NADINE ELIXIR -100 ML ", precio: "$5.250,00", imagen: "./img/img-perfumesFemeninos/2.jpeg", url:"https://wa.link/6zacdi" },
+    { nombre: "F05 Yd Yves D’Orgeval Parfums Fem. Ad Le Secret Elixir – 100 ML ", precio: "$5.250,00", imagen: "./img/img-perfumesFemeninos/3.jpeg", url:"https://wa.link/3xi9t3" },
+    { nombre: "F06 Yd Yves D’Orgeval Parfums Femenino Ad Le Secret – 100 ML ", precio: "$5.250,00", imagen: "./img/img-perfumesFemeninos/4.jpeg", url:"https://wa.link/2ulipv" },
+    { nombre: "F07 Yd Yves D’Orgeval Parfums Femenino Ad – 100 ML ", precio: "$5.250,00", imagen: "./img/img-perfumesFemeninos/5.jpeg", url:"https://wa.link/thvvd5" },
+    { nombre: "F08 Yd Yves D’Orgeval Parfums Femenino Miu Miu – 100 ML5510 ", precio: "$5.250,00", imagen: "./img/img-perfumesFemeninos/6.jpeg", url:"https://wa.link/7wjih5" },
+    { nombre: "F10 Yd Yves D’Orgeval Parfums Femenino Lovely Jenko – 70 ML4585 ", precio: "$4.585,00", imagen: "./img/img-perfumesFemeninos/7.jpeg", url:"https://wa.link/es3xlc" },
+    { nombre: "F11 Yd Yves D’Orgeval Parfums Femenino Gh – 50 ML ", precio: "$4.585,00", imagen: "./img/img-perfumesFemeninos/8.jpeg", url:"https://wa.link/mwb211" },
+    { nombre: "F13 Yd Yves D’Orgeval Parfums Femenino Yd Fleur – 100 ML ", precio: "$5.350,00", imagen: "./img/img-perfumesFemeninos/9.jpeg", url:"https://wa.link/598ns3" },
+    { nombre: "F14 Yd Yves D’Orgeval Parfums Femenino Lumiere – 110 ML ", precio: "$5.350,00", imagen: "./img/img-perfumesFemeninos/10.jpeg", url:"https://wa.link/gnleb5" },
+    { nombre: "F15 Yd Yves D’Orgeval Parfums Femenino LuMieRe Black – 110 ML ", precio: "$4.835,00", imagen: "./img/img-perfumesFemeninos/11.jpeg", url:"https://wa.link/cv2hs1" },
+    { nombre: "F16 Yd Yves D´Orgeval Parfums Femenino  Spell Night – 80 ML ", precio: "$4.835,00", imagen: "./img/img-perfumesFemeninos/12.jpeg", url:"https://wa.link/v84p5g" },
+    { nombre: "F17 Yd Yves D’Orgeval Parfums Fem Spell’s Night Blue Drop-80 ML ", precio: "$4.835,00", imagen: "./img/img-perfumesFemeninos/13.jpeg", url:"https://wa.link/4nw54y" },
+    { nombre: "F18 Yd Yves D’Orgeval Parfums Femenino – D’Orgeval Blue – 100 ML ", precio: "$4.835,00", imagen: "./img/img-perfumesFemeninos/14.jpeg", url:"https://wa.link/3h0o81" },
+    { nombre: "F19 Yd Yves D’Orgeval Parfums Femenino Hvngary – 100 ML ", precio: "$5.325,00", imagen: "./img/img-perfumesFemeninos/15.jpeg", url:"https://wa.link/207db3" },
+    { nombre: "F20 Yd Yves D’Orgeval Parfums Femenino Yd Ange – 80 ML ", precio: "$5.715,00", imagen: "./img/img-perfumesFemeninos/16.jpeg", url:"https://wa.link/bsnss2" },
+    { nombre: "F21 Yd Yves D’Orgeval Parfums Femenino Yd Irresistible – 100 ML ", precio: "$5.665,00", imagen: "./img/img-perfumesFemeninos/17.jpeg", url:"https://wa.link/l2pjn8" },
+    { nombre: "F22 Yd Yves D’Orgeval Parfums  Yd Irresistible Sensual – 80 ML ", precio: "$5.505,00", imagen: "./img/img-perfumesFemeninos/18.jpeg", url:"https://wa.link/hwy51f" },
+    { nombre: "F24 Yd Yves D´Orgeval Parfums Fem. D’Orgeval Classic – 100 ML ", precio: "$5.505,00", imagen: "./img/img-perfumesFemeninos/19.jpeg", url:"https://wa.link/711wdv" },
+    { nombre: "F25 Yd YvesD’Orgeval Parfums Femenino Orixa – 100 ML ", precio: "$5.505,00", imagen: "./img/img-perfumesFemeninos/20.jpeg", url:"https://wa.link/lzdywj" },
+    { nombre: "F27 YD Yves D’Orgeval Parfums Femenino M.M Eau My Gold 75 ML ", precio: "$4.845,00", imagen: "./img/img-perfumesFemeninos/21.jpeg", url:"https://wa.link/tueitf" },
+    { nombre: "F28 Yd Yves D’Orgeval Parfums Femenino Miss Millonaire – 100 ML ", precio: "$4.845,00", imagen: "./img/img-perfumesFemeninos/22.jpeg", url:"https://wa.link/mkckx5" },
+    { nombre: "F29 Yd Yves D’Orgeval Parfums Fem. M. Million Prive – 90 ML ", precio: "$5.350,00", imagen: "./img/img-perfumesFemeninos/24.jpeg", url:"https://wa.link/j4negv" },
+    { nombre: "F30 Yd Yves D’Orgeval Parfums Fem.  D’Orgeval Code – 100 ML ", precio: "$5.415,00", imagen: "./img/img-perfumesFemeninos/25.jpeg", url:"https://wa.link/ppehz1" },
+    { nombre: "F31 Yd Yves D’Orgeval Parfums Fem Baiser D’Amour – 90 ML ", precio: "$5.415,00", imagen: "./img/img-perfumesFemeninos/26.jpeg", url:"https://wa.link/y2pw5d" },
+    { nombre: "F32 Yd Yves D’Orgeval Parfums Fem Nadine L’eau – 100 ML ", precio: "$5.250,00", imagen: "./img/img-perfumesFemeninos/27.jpeg", url:"https://wa.link/83mjsf" },
+    { nombre: "F33 Yd Yves D’Orgeval Parfums Femenino Nadine – 100 ML ", precio: "$5.350,00", imagen: "./img/img-perfumesFemeninos/28.jpeg", url:"https://wa.link/cl3d3c" },
+    { nombre: "F34 Yd Yves D’Orgeval Parfums Femenino Sutil – 75 ML ", precio: "$5.510,00", imagen: "./img/img-perfumesFemeninos/29.jpeg", url:"https://wa.link/jwvw94" },
+    { nombre: "F35 Yd Yves D’Orgeval Parfums Femenino Hai – 100 ML ", precio: "$5.325,00", imagen: "./img/img-perfumesFemeninos/30.jpeg", url:"https://wa.link/q6bbd1" },
+    { nombre: "F36 Yd Yves D’Orgeval Parfums Femenino Yd’Licious – 100 ML ", precio: "$5.325,00", imagen: "./img/img-perfumesFemeninos/31.jpeg", url:"https://wa.link/cbzhd3" },
+  ];
+  
+  function mostrarTarjetas() {
+    const contenedorTarjetas = document.getElementById("contenedor-tarjetas");
+  
+    let tarjetasHTML = "";
+    productosHombre.forEach((producto) => {
+      tarjetasHTML += `
+      <div class="centerCard">
+    <div class="card" style="width: 18rem;">
+      <img src="${producto.imagen}" class="card-img-top" alt="...">
+      <div class="card-body body-center">
+        <h5 class="card-title">${producto.nombre}</h5>
+        <p class="card-text">${producto.precio}</p>
+        <a href="${producto.url}" class="btn btn-primary button-color">Comprar</a>
+      </div>
+    </div>
+  </div>
+          `;
+    });
+  
+    contenedorTarjetas.innerHTML = tarjetasHTML;
+  }
+  
+  window.addEventListener("load", mostrarTarjetas);
+  //! PRODUCTOS FEMENINO
+
+  // ! search
+const form = document.getElementById('ctn-search');
+const input = document.getElementById('input-search');
+const resultsContainer = document.getElementById('results');
+const modal = document.getElementById('myModal');
+const closeModalButton = document.getElementById('closeModal');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const searchTerm = input.value.toLowerCase();
+  const filteredProducts = productosHombre.filter(producto => {
+    const productName = producto.nombre.toLowerCase();
+    return productName.includes(searchTerm);
+  });
+
+  displayResults(filteredProducts);
+  openModal();
+});
+
+closeModalButton.addEventListener('click', closeAndClearModal);
+
+function displayResults(products) {
+  resultsContainer.innerHTML = '';
+
+  if (products.length === 0) {
+    resultsContainer.textContent = 'No se encontraron productos.';
+  } else {
+    products.forEach(producto => {
+      const productCard = document.createElement('div');
+      productCard.classList.add('product-card');
+
+      const productImage = document.createElement('img');
+      productImage.src = producto.imagen;
+
+      const productName = document.createElement('h2');
+      productName.textContent = producto.nombre;
+
+      const productPrice = document.createElement('p');
+      productPrice.textContent = `Precio: ${producto.precio}`;
+
+      productCard.appendChild(productImage);
+      productCard.appendChild(productName);
+      productCard.appendChild(productPrice);
+
+      resultsContainer.appendChild(productCard);
+    });
+  }
+}
+
+function openModal() {
+  modal.style.display = 'block';
+}
+
+function closeAndClearModal() {
+  modal.style.display = 'none';
+  resultsContainer.innerHTML = '';
+}
+// ! search
